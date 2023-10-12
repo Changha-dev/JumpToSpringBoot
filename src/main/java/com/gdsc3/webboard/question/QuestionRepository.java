a328bbcc60041d37a4d26bd9146d121a262795bc
changha <mentenseoul@gmail.com>
2023-10-12 22:53:15 +0900

    add : week_04

--- src/main/java/com/gdsc3/webboard/question/QuestionRepository.java
+++ src/main/java/com/gdsc3/webboard/question/QuestionRepository.java
@@ -1,7 +1,11 @@
 package com.gdsc3.webboard.question;
 
 import com.gdsc3.webboard.question.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    Page<Question> findAll(Pageable pageable);
 }
